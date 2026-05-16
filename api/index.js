ce0f2d5194932a29f51480f540b59c245210e422
pfreezv <81359217+pfreezv@users.noreply.github.com>
2026-05-16 13:04:29 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -111,7 +111,7 @@ Recomendaciones preanestésicas:
 </datos_automatizacion>
 `;
 
-  const url = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent';
+  const url = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent';
 
   try {
     // 5. Envío de la petición estructurada hacia la API de Google
